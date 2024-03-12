d994527bb44c8919643c1e17cd5f89709e591357
Gurjit Singh <gurjits081@gmail.com>
2024-03-12 22:37:29 +0530

    Added react-router & a details component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { createRoot } from "react-dom/client";
-import SearchParams from './SearchParams';
-
+import SearchParams from "./SearchParams";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
-  )
+    <BrowserRouter>
+      <header>
+        <Link to="/">
+          <h1>Adopt Me!</h1>
+        </Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 const container = document.getElementById("root");
