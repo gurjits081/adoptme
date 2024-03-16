93b3f5f55c81a56b54b7a3886cef8dfe57c1c3d3
Gurjit Singh <gurjits081@gmail.com>
2024-03-16 12:38:02 +0530

    Added error boundary

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import ErrorBoundary from "./ErrorBoundary";
+import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
 
 const Details = () => {
@@ -19,6 +21,7 @@ const Details = () => {
 
     return (
       <div className="details">
+        <Carousel images={pet.images} />
         <div>
           <h1>{pet.name}</h1>
           <h2>{pet.animal} - {pet.breed} - {pet.state}
@@ -29,8 +32,16 @@ const Details = () => {
       </div>
     )
   };
+
+  function DetailsErrorBoundary(props) {
+    return (
+      <ErrorBoundary>
+        <Details {...props}/>
+      </ErrorBoundary>
+    )
+  }
   
-  export default Details;
+  export default DetailsErrorBoundary;
 
 
 
